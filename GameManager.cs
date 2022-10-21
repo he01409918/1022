6b65c1acb6d2357e8bf1088009c9c121ad652823
he01409918 <92556503+he01409918@users.noreply.github.com>
2022-10-21 11:26:51 +0800

    Add files via upload

--- GameManager.cs
+++ GameManager.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager Instance;
     [Header("分數")]
     public int score;
+    private void Start()
+    {
+        Instance = this;
+    }
+
+    public void OnAddScore(int value)
+    {
+        score += value;
+    }
 }
---
+++ ScoreTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreTrigger : MonoBehaviour
+{
+    [Header("分數")]
+    public int score;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Ball"))
+        {
+            GameManager.Instance.OnAddScore(score);
+        }
+    }
+}
