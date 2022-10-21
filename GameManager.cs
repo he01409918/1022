115ff07d1bab75f2568e0d95c267603f66043777
he01409918 <92556503+he01409918@users.noreply.github.com>
2022-10-21 11:54:10 +0800

    Update GameManager.cs

--- GameManager.cs
+++ GameManager.cs
@@ -1,19 +1,23 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
     [Header("分數")]
     public int score;
+
+    public Text scoreText;
     private void Start()
     {
         Instance = this;
     }
 
-    public void OnAddScore(int value)
+    public void OnAddScore(int value) 
     {
         score += value;
+        scoreText.text = score.ToString();
     }
 }
