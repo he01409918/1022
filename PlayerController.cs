d21ca9c2c19ba1da812d94fd077a1f5b0effa68e
he01409918 <92556503+he01409918@users.noreply.github.com>
2022-10-28 10:41:54 +0800

    Add files via upload

---
+++ PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class PlayerController : MonoBehaviourPun
+{
+    void Start()
+    {
+        if (photonView.IsMine)
+        {
+            Debug.LogError("我自己");
+        }
+        else
+        {
+            Debug.LogError("別的玩家");
+        }
+    }
+}
