029e82f384fc2e1486975b9c10a3085fb1818312
he01409918 <92556503+he01409918@users.noreply.github.com>
2022-10-28 09:41:52 +0800

    Add files via upload

---
+++ NetworkConnect.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class NetworkConnect : MonoBehaviourPunCallbacks
+{
+    public static NetworkConnect Instance;
+    public Transform vrHeadPos;
+    public Transform vrRightHandPos;
+    public Transform vrLeftHandPos;
+
+    private void Start()
+    {
+        Instance = this;
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinLobby();
+        Debug.LogError("OnConnectedToMaster");
+    }
+    public override void OnJoinedLobby()
+    {
+        PhotonNetwork.JoinRandomOrCreateRoom();
+        Debug.LogError("OnJoinedLobby");
+    }
+    public override void OnJoinedRoom()
+    {
+        PhotonNetwork.Instantiate("Player", Vector3.zero, Quaternion.identity);
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+    }
+    public override void OnPlayerLeftRoom(Player otherPlayer)
+    {
+    }
+}
